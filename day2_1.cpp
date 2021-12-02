7905d8e1004e6a3f21b628cd2c4b92c0ffce3184
JoaoSilva1 <joao.silva1@ua.pt>
2021-12-02 18:55:01 +0100

    second day challenges

---
+++ day2_1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+
+
+void get_pos(string file_name,int output[]){
+    
+    ifstream infile(file_name);
+    
+    
+    string line;
+    
+
+    int horizontal = 0;
+    int vertical = 0;
+
+    
+
+    while(getline(infile, line)){
+        //cout << line[0] << endl;
+        switch(line[0]) {
+            case 'f':
+                horizontal += line[line.length() - 1] - 48;
+            break;
+            case 'u':
+                vertical -= line[line.length() - 1] - 48;
+            break;
+            case 'd':
+                vertical += line[line.length() - 1] - 48;
+            break;
+            default:
+                cout << "ERROR" << endl;
+        }    
+        //cout << horizontal << endl;
+        //cout << vertical << endl;
+
+           
+    }
+
+    output[0] = horizontal;
+    output[1] = vertical;
+
+}
+
+
+int main(){
+
+    int output[2];
+    get_pos("input_2.txt", output);
+
+    cout << output[0]*output[1] << endl; 
+    
+    return 0;
+}
